8ffa024de23c2935e53f7e1871278c22d363012b
Daniel Berger <djberg96@gmail.com>
2022-07-04 22:04:02 -0400

    Update ntp.conf read handling, minor output updates.

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -3,17 +3,21 @@ require 'attempt'
 
 Gem.pre_install do |installer|
   offset = 0
+  default = 'pool.ntp.org'
 
-  if Gem.win_platform?
-    server = 'pool.ntp.org' # TODO: Find better default?
-  else
-    server = IO.read("/etc/ntp.conf")[/^server\s([\p{Alnum}\.]*)/, 1] || 'pool.ntp.org'
+  begin
+    server = File.read("/etc/ntp.conf")[/^server\s([\p{Alnum}\.]*)/, 1] || default
+  rescue Errno::ENOENT
+    server = default
   end
 
+  # TODO: Make this configurable as a flag to the install command.
   attempt(tries: 3, interval: 3) do
+    puts "Checking for clock skew..."
     offset = Net::NTP.get(server, 'ntp', 5).offset.abs
   end
 
+  # TODO: Make this configurable as a flag to the install command.
   if offset > 300
     msg = %Q{
 Your clock appears to be skewed by 5 or more minutes. Gem installation attempts may fail.
@@ -23,11 +27,11 @@ in order to get your computer's clock synced before proceeding.
 }
 
     puts msg
-    print "\nProceed with gem installation? (y/n): "
+    print "\nProceed with gem installation? (y/N): "
 
     case choice = $stdin.gets.chomp
       when /\Ay/i
-      when /\An/i then next false
+      when /\An|^$/i then next false
       else fail "cannot understand '#{choice}'"
     end
   else
